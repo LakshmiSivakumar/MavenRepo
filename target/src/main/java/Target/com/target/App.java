053feca81e716c8c56e73d5732ba7f4cce4dabf9
LakshmiSivakumar <lakshmi.sivakumar13@gmail.com>
2019-04-03 00:39:44 +0530

    first checkin

---
+++ target/src/main/java/Target/com/target/App.java
@@ -0,0 +1,13 @@
+package Target.com.target;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
